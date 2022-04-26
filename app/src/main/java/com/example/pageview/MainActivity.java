b17e26fb36f5c4020929ba1f1ff7b65ebb7d9694
HP <zainabkhadly@gmail.com>
2022-04-26 20:25:02 +0000

    initial

---
+++ app/src/main/java/com/example/pageview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.pageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
+}
